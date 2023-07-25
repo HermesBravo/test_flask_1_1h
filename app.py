a63d4c94fd5393c7d2373dee5e8f2f4c3a32cf09
Hermes <131220852+HermesBravo@users.noreply.github.com>
2023-07-25 16:46:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,5 @@ CORS(app)
 def main():
     return {"payload":"welcome to my project"}
 
-@app.route("/read")
-def read():
-    return {"payload":"read successfully"} 
-
-@app.route("/create", methods = ["POST"])
-def create():
-    return {"payload":"create successfully"} 
-
-@app.route("/delete", methods = ["DELETE"])
-def delete():
-    return {"payload":"delete successfully"} 
-
-@app.route("/put", methods = ["PUT"])
-def put():
-    return {"payload":"put successfully"} 
-
-
-
 if __name__ == "__main__":
     app.run(debug=True) 
